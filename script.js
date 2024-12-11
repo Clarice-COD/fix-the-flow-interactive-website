edca959b555b61e72d61025ee2a05da74c9b1411
Clarice-COD <clariceboerlijst@gmail.com>
2024-12-11 12:43:28 +0100

    Formulier werkend gemaakt

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var button = document.querySelector("button")
+// Doe bovenstaande voor elke input op de pagina..
 
-button.addEventListener("click", function(){
-    element.classList.toggle ("yay");
-});
 
+// QuerySelectorAll zorgt dat alle elementen die de input hebben worden geselecteerd
+let alleInputsOpDePagina = document.querySelectorAll('input')
+
+// forEach zorgt ervoor dat elke input een functie krijgt
+alleInputsOpDePagina.forEach(function(huidigeInput) {
+    huidigeInput.addEventListener('invalid', function (){
+        huidigeInput.setCustomValidity('Dit veld is nog niet ingevuld')
+    });
+    
+    huidigeInput.addEventListener('input', function() {
+        huidigeInput.setCustomValidity('')
+    })
+})
